e1b598e8532b0212a22fd8f72223fbb5ba665385
Yogi Sharma <yogeshwer@fb.com>
2015-02-02 17:22:09 -0800

    Initial Commit

---
+++ TipCalculator2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator2
+//
+//  Created by Yogi Sharma on 2/2/15.
+//  Copyright (c) 2015 Yogi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator2
+//
+//  Created by Yogi Sharma on 2/2/15.
+//  Copyright (c) 2015 Yogi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
