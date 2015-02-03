d519538b432733dc39564dcf3698d5736b1f00fd
Yogi Sharma <yogeshwer@fb.com>
2015-02-02 20:12:29 -0800

    cp

---
+++ TipCalculator2/TipViewController.m
@@ -0,0 +1,61 @@
+//
+//  TipViewController.m
+//  TipCalculator2
+//
+//  Created by Yogi Sharma on 2/2/15.
+//  Copyright (c) 2015 Yogi. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
+- (IBAction)onTap:(id)sender;
+- (void)updateValues;
+
+@end
+
+@implementation TipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    if (self) {
+        self.title = @"Tip Calculator";
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)onTap:(id)sender {
+    // [self.view endEditing:YES];
+    [self updateValues];
+}
+
+- (void)updateValues {
+    float billAmount = [self.billTextField.text floatValue];
+    NSArray *tipValues = @[@(0.15), @(0.18), @(0.2)];
+    float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    float totalAmount = tipAmount + billAmount;
+    self.tipLabel.text = [NSString stringWithFormat:@"%0.2f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"%0.2f", totalAmount];
+}
+@end
